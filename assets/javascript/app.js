762a928c798e7d4f4e2e9c6a2a490c491f79e90f
mrabanda <mrabanda@gmail.com>
2016-12-24 11:50:25 -0500

    buttons generate imgs, animate gifs when clicked, stop when clicked again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 var topics = ["car", "bus", "truck", "train", "trolley", "highway", "subway", "street", "road", "tunnel", "underground", "bridge", "traffic", "congestion",];
-var newTopic;
-var newButton;
+var newTopic, newButton;
 
 function genNewButton() {
   newButton = $("<button>").addClass("btn topic").attr("data-name", newTopic).text(newTopic);
@@ -27,9 +26,38 @@ function addbutton(event) {
   };
 };
 
-$("#add-topic").on("click", addbutton);
+function displayGifs() {
+  $("#giphyPics").empty();
+  var gifSearch = $(this).attr("data-name");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=dc6zaTOxFJmzC";
 
-// Adding click event listeners to all elements with a class of "topic"
-// $(document).on("click", ".newTopic", displaytopicInfo);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response)
+    for(j=0;j<10;j++) {
+      gifStillUrl = response.data[j].images.fixed_width_still.url
+      gifUrl = response.data[j].images.fixed_width.url
+      var gifImageTag = $("<img>").attr({src: gifStillUrl, "data-still":gifStillUrl, "data-gif": gifUrl, alt: gifSearch});
+      $("#giphyPics").append(gifImageTag);
+    }
+  });
+
+}
+
+function getGiphy() {
+  if($(this).attr("src") !== $(this).attr("data-gif")) {
+    var newSrc = $(this).attr("data-gif");
+    $(this).attr("src", newSrc); 
+  } else {
+    var newSrc = $(this).attr("data-still");
+    $(this).attr("src", newSrc);   
+  };
+};
+
+$("#add-topic").on("click", addbutton);
+$(document).on("click", ".topic", displayGifs);
+$(document).on("click", "img", getGiphy);
 
 renderButtons();
